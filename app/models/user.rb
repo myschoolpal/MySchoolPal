5ec4751105bd2b26f364c365f88d6e6fb045c4d7
myschoolpal <steve.howse@myschoolpal.com>
2013-12-01 19:14:39 +0000

    updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :timeoutable
+  devise :database_authenticatable, :registerable, :timeoutable,
          :recoverable, :rememberable, :trackable, :validatable, :timeout_in => 15.minutes
 		 
 		 has_one :user_info, dependent: :destroy
