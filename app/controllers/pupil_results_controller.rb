bcbe50252c3b042814e44a5af27d56caab328aa2
myschoolpal <steve.howse@myschoolpal.com>
2013-12-01 18:07:21 +0000

    updates

--- app/controllers/pupil_results_controller.rb
+++ app/controllers/pupil_results_controller.rb
@@ -88,13 +88,13 @@ class PupilResultsController < ApplicationController
 	@levels =Array.new
 	
 	if params[:key_stage] == "ks1_maths"
-		@key_stage = a.ks1_maths
+		@key_stage = "a.ks1_maths"
 	elsif params[:key_stage] == "ks1_writing"
-		@key_stage = a.ks1_writing
+		@key_stage = "a.ks1_writing"
 	elsif params[:key_stage] == "ks1_reading"
-		@key_stage = a.ks1_reading
+		@key_stage = "a.ks1_reading"
 	else params[:key_stage] == "ks1_english"
-		@key_stage = a.ks1_english
+		@key_stage = "a.ks1_english"
 	
 	end
 	@year_id = params[:year_id]
