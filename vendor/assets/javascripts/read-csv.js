a063821cfc37a53d97bd70fc3a712a0dd7354d0a
myschoolpal <steve.howse@myschoolpal.com>
2014-05-26 21:35:12 +0100

    updates

--- vendor/assets/javascripts/read-csv.js
+++ vendor/assets/javascripts/read-csv.js
@@ -1,4 +1,52 @@
+var surname = [];
+var forename = [];
+var target = [];
+var initial = [];
+var result = [];
+var target_result = [];
+var initial_result = [];
+var comparison = [];
+var result_number = [];
+var target_number = [];
+var levels_progress = {};
+var levels_target = {};
+var grade_array = ['W','1C','1B','1A','2C','2B','2A','3C','3B','3A','4C','4B','4A','5C','5B','5A','6C','6B','6A','7C','7B','7A','8C','8B','8A'];
+var aps_array = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29"
+				,"30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53"];
+var lines = [];
+
+function convertAps(aps) {
+	if(aps<6) {	return "W";	}
+	else if (aps == 7 || aps == 8) {return "1C";}
+	else if (aps == 9 || aps == 10) {return "1B";}
+	else if (aps == 11 || aps == 12) {return "1A";}
+	else if (aps == 13 || aps == 14) {return "2C";}
+	else if (aps == 15 || aps == 16) {return "2B";}
+	else if (aps == 17 || aps == 18) {return "2A";}
+	else if (aps == 19 || aps == 20) {return "3C";}
+	else if (aps == 21 || aps == 22) {return "3B";}
+	else if (aps == 23 || aps == 24) {return "3A";}
+	else if (aps == 25 || aps == 26) {return "4C";}
+	else if (aps == 27 || aps == 28) {return "4B";}
+	else if (aps == 29 || aps == 30) {return "4A";}
+	else if (aps == 31 || aps == 32) {return "5C";}
+	else if (aps == 33 || aps == 34) {return "5B";}
+	else if (aps == 35 || aps == 36) {return "5A";}
+	else if (aps == 37 || aps == 38) {return "6C";}
+	else if (aps == 39 || aps == 40) {return "6B";}
+	else if (aps == 41 || aps == 42) {return "6A";}
+	else if (aps == 43 || aps == 44) {return "7C";}
+	else if (aps == 45 || aps == 46) {return "7B";}
+	else if (aps == 47 || aps == 48) {return "7A";}
+	else if (aps == 49 || aps == 50) {return "8C";}
+	else if (aps == 51 || aps == 52) {return "8B";}
+	else if (aps == 53) {return "8a";}
+}
+
+
 function handleFiles(evt) {
+	example = false;
+	lines = [];
 	// Check for the various File API support.
 	var files = evt.target.files; 
 	if (window.FileReader) {
@@ -22,23 +70,53 @@ function loadHandler(event) {
 	var csv = event.target.result;
 	processData(csv);             
 }
-	var surname = [];
-	var forename = [];
-	var target = [];
-	var initial = [];
-	var result = [];
-	var target_result = [];
-	var initial_result = [];
-	var levels_progress = {};
-	var levels_target = {};
-	var grade_array = ['U','G','F','E','D','C','B','A','A*','W','1C','1B','1A','2C','2B','2A','3C','3B','3A','4C','4B','4A','5C','5B','5A','6C','6B','6A','7C','7B','7A','8C','8B','8A'];
+	
+	
+	function exampleData(){
+	example = true;
+	lines = [];
+	document.getElementById("files").value = "";
+	lines[0] = ['Surname', "Forename","Target","KS1","Result"];
+	lines[1] = ['Smith', "Daniel","21","20","89"];
+	lines[2] = ['Knight', "Gemma","4a","1b","2c"];
+	lines[3] = ['Colbert', "John","3b","1b","4c"];
+	lines[4] = ['Austin', "Edward","2a","1a","3b"];
+	lines[5] = ['Lynch', "Amber","3c","2b","2a"];
+	lines[6] = ['Thompson', "Lynda","3a","2c","3a"];
+	lines[7] = ['Dean', "Melissa","4b","2a","4a"];
+	lines[8] = ['Mills', "Mark","4c","1c","5c"];
+	lines[9] = ['Hudson', "David","4a","1c","4b"];
+	lines[10] = ['Reeves', "Cameron","5c","1b","5b"];
+	lines[11] = ['Andrews', "Kate","4a","1b","4b"];
+	lines[12] = ['Robinson', "Bradley","4b","2a","4c"];
+	lines[13] = ['Hinch', "Lauren","5a","2b","5b"];
+	lines[14] = ['Brannan', "Joanne","5b","2b","5b"];
+	lines[15] = ['Walker', "Michelle","4b","2a","4a"];
+	lines[16] = ['Humphreys', "Laura","5c","2b","5b"];
+	lines[17] = ['Munnings', "Louise","5b","2b","4a"];
+	lines[18] = ['Storey', "Alex","3a","2c","4b"];
+	lines[19] = ['Clausen', "Rachel","4b","2a","4a"];
+	lines[20] = ['Sinclair', "Mike","4b","1b","3a"];
+	
+	
+	processData();
+	}
+	
+	
+	function isInArray(value, array) {
+		return array.indexOf(value) > -1;
+	}
+	
+	var array_test = [1,2,3,4];
+	
+	
 	function processData(csv) {
-    var allTextLines = csv.split(/\r\n|\n/);
-    var lines = [];
+    if(example != true) {
+	var allTextLines = csv.split(/\r\n|\n/);
     while (allTextLines.length) {
         lines.push(allTextLines.shift().split(','));
     }
-	
+	}
 	for (var i = 1; i < lines.length; i++) {
 		if(lines[i][0].length >0) {
 			surname.push(lines[i][0]);
@@ -47,11 +125,64 @@ function loadHandler(event) {
 			initial.push(lines[i][3]);
 			result.push(lines[i][4]);
 			if(lines[i][4] && lines[i][3]) {
-			initial_result.push(grade_array.indexOf(lines[i][4].toUpperCase())-grade_array.indexOf(lines[i][3].toUpperCase()));
+				if(isInArray(lines[i][4].toUpperCase(),grade_array) == true) {
+					var result_value = grade_array.indexOf(lines[i][4].toUpperCase())
+					result_number.push(result_value);
+				}
+				else if (isInArray(lines[i][4],aps_array) == true) {
+					var convert_aps = convertAps(lines[i][4])
+					var result_value = grade_array.indexOf(convert_aps);
+					result_number.push(result_value);
+				}
+				else {
+					result_value = 0;
+					result_number.push(result_value);
+				}
+				if(isInArray(lines[i][3].toUpperCase(),grade_array) == true) {
+					var initial_value = grade_array.indexOf(lines[i][3].toUpperCase())
+					comparison.push(initial_value);
+				}
+				else if (isInArray(lines[i][3],aps_array) == true) {
+					var convert_aps = convertAps(lines[i][3])
+					var initial_value = grade_array.indexOf(convert_aps);
+					comparison.push(initial_value);
+				}
+				else {
+					initial_value = 0;
+					comparison.push(initial_value);
+				}
+			
+				initial_result.push(result_value-initial_value);
+			
 			}
+			
 			if(lines[i][4] && lines[i][2]) {
-			target_result.push(grade_array.indexOf(lines[i][4].toUpperCase())-grade_array.indexOf(lines[i][2].toUpperCase()));
+				if(isInArray(lines[i][4].toUpperCase(),grade_array) == true) {
+				var result_value = grade_array.indexOf(lines[i][4].toUpperCase())
+				}
+				else if (isInArray(lines[i][4],aps_array) == true) {
+				var result_value = aps_array.indexOf(lines[i][4]);
+				}
+				else {
+					result_value = 0;
+				}
+				if(isInArray(lines[i][2].toUpperCase(),grade_array) == true) {
+				var target_value = grade_array.indexOf(lines[i][2].toUpperCase())
+				target_number.push(target_value);
+				}
+				else if (isInArray(lines[i][2],aps_array) == true) {
+				var convert_aps = convertAps(lines[i][2])
+				var target_value = grade_array.indexOf(convert_aps);
+				target_number.push(target_value);
+				}
+				else {
+					target_value = 0;
+					target_number.push(target_value);
+				}
+			target_result.push(result_value-target_value);
+			
 			}
+			
 
 		}
 	}
@@ -88,13 +219,12 @@ function loadHandler(event) {
 
 
 function pieChart_levels() {
-
-
+ 
   var data = [];
    for (i=-50; i<51; i++) {
-	data.push([i +' LP', levels_progress[i]]);
+	data.push([i +' Levels', levels_progress[i]]);
 	}
-  
+   
   var plot1 = jQuery.jqplot ('pie_chart', [data], 
     { 
 		title: 'Levels of Progress',
@@ -114,6 +244,7 @@ function pieChart_levels() {
   
 	
 	function print_PieChart(){
+		$('#pie_chart_img').empty();
 		var imgData = $('#pie_chart').jqplotToImageStr({});
 		var imgElem = $('<img/>').attr('src', imgData); 
 		
@@ -125,7 +256,7 @@ function pieChart_levels() {
 		
         //Reset the page's HTML with div's HTML only
         print_window.document.write( 
-          "<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"css/main.css\" /><link rel=\"stylesheet\" type=\"text/css\" href=\"css/bootstrap/bootstrap.min.css\" /><title></title></head><body>" + 
+          "<html><head><title></title></head><body>" + 
           divElements + "</body>");
 
         //Print Page
@@ -165,6 +296,7 @@ function pieChart_target() {
       plot2.replot();
 	 
 	function print_PieChart_target(){
+		$('#pie_chart_target_hidden').empty();
 		var imgData = $('#pie_chart_target').jqplotToImageStr({});
 		var imgElem = $('<img/>').attr('src', imgData); 
 		$('#pie_chart_target_hidden').append(imgElem);
@@ -173,7 +305,7 @@ function pieChart_target() {
 		
         //Reset the page's HTML with div's HTML only
         print_window.document.write( 
-          "<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"css/main.css\" /><link rel=\"stylesheet\" type=\"text/css\" href=\"css/bootstrap/bootstrap.min.css\" /><title></title></head><body>" + 
+          "<html><head><title></title></head><body>" + 
           divElements + "</body>");
 
         //Print Page
@@ -189,30 +321,9 @@ function pieChart_target() {
 
 
 function barChart_target(lines) {
-    var s1 = [];
-	for (i=1; i<lines.length; i++) {
-		if (lines[i][3] === undefined) {
-		}
-		else {
-		s1.push(grade_array.indexOf(lines[i][3].toUpperCase()));
-		}
-	 }
-	var s2 = [];
-	for (i=1; i<lines.length; i++) {
-		if (lines[i][4] === undefined) {
-		}
-		else {
-		s2.push(grade_array.indexOf(lines[i][4].toUpperCase()));
-		}
-	 }
-    var s3 = [];
-	 for (i=1; i<lines.length; i++) {
-		if (lines[i][2] === undefined) {
-		}
-		else {
-		s3.push(grade_array.indexOf(lines[i][2].toUpperCase()));
-		}
-	 }
+    var s1 = comparison;
+	var s2 = result_number;
+	var s3 = target_number;
 	
     // Can specify a custom tick Array.
     // Ticks should match up one for each y value (category) in the series.
@@ -221,45 +332,45 @@ function barChart_target(lines) {
 		ticks.push([lines[i][0] + '<br>'+ lines[i][1]]);
 	 }
 	 	 tickFormatter = function (format, val) { 
-	 if(val==8)
+	 if(val==-1)
 	 {return "No Grade";}
-	 else if(val==9)
+	 else if(val==0)
 	 {return "W";}
-	else if(val==10)
+	else if(val==1)
 	 {return "1C";}
-	 else if(val==11)
+	 else if(val==2)
 	 {return "1B";}
-	 else if(val==12)
+	 else if(val==3)
 	 {return "1A";}
-	 else if(val==13)
+	 else if(val==4)
 	 {return "2C";}
-	 else if(val==14)
+	 else if(val==5)
 	 {return "2B";}
-	 else if(val==15)
+	 else if(val==6)
 	 {return "2A";}
-	 else if(val==16)
+	 else if(val==7)
 	 {return "3C";}
-	 else if(val==17)
+	 else if(val==8)
 	 {return "3B";}
-	  else if(val==18)
+	  else if(val==9)
 	 {return "3A";}
-	  else if(val==19)
+	  else if(val==10)
 	 {return "4C";}
-	  else if(val==20)
+	  else if(val==11)
 	 {return "4B";}
-	  else if(val==21)
+	  else if(val==12)
 	 {return "4A";}
-	  else if(val==22)
+	  else if(val==13)
 	 {return "5C";}
-	  else if(val==23)
+	  else if(val==14)
 	 {return "5B";}
-	  else if(val==24)
+	  else if(val==15)
 	 {return "5A";}
-	  else if(val==25)
+	  else if(val==16)
 	 {return "6C";}
-	 else if(val==26)
+	 else if(val==17)
 	 {return "6B";}
-	 else if(val==27)
+	 else if(val==18)
 	 {return "6A";}
   }
 
@@ -327,7 +438,7 @@ function barChart_target(lines) {
 		  formatter: tickFormatter
 		  },
 		    tickInterval : 1,
-			min: 8
+			min: 0
             },
 			 y2axis: {
 			   pad: 1.05,
@@ -368,18 +479,22 @@ function drawOutput(lines){
 			var firstNameCell = row.insertCell(-1);
 			if (i>0 && j==4) {
 				if(lines[i][j] && lines[i][j-2]) {
-				if(grade_array.indexOf(lines[i][j].toUpperCase())>grade_array.indexOf(lines[i][j-2].toUpperCase())) {
-				firstNameCell.className = 'above_target';
-				}
-				if(grade_array.indexOf(lines[i][j].toUpperCase())==grade_array.indexOf(lines[i][j-2].toUpperCase())) {
-				firstNameCell.className = 'on_target';
-				}
-				if(grade_array.indexOf(lines[i][j].toUpperCase())==((grade_array.indexOf(lines[i][j-2].toUpperCase()))-1)) {
-				firstNameCell.className = 'one_below_target';
-				}
-				if(grade_array.indexOf(lines[i][j].toUpperCase())<((grade_array.indexOf(lines[i][j-2].toUpperCase()))-1)) {
-				firstNameCell.className = 'below_target';
-				}
+				
+					if(target_result[i-1]>0) {
+						firstNameCell.className = 'above_target';
+					}
+					if(target_result[i-1]==0) {
+						firstNameCell.className = 'on_target';
+					}
+					if(target_result[i-1]==-1) {
+						firstNameCell.className = 'one_below_target';
+					}
+					if(target_result[i-1]<-1) {
+						firstNameCell.className = 'below_target';
+					}
+					if(isInArray(lines[i][j].toUpperCase(), grade_array) == false && isInArray(lines[i][j], aps_array) == false) {
+						firstNameCell.className = 'incorrect_result';
+					}
 				}
 				else {
 				firstNameCell.className = 'no_color';
@@ -394,7 +509,7 @@ function drawOutput(lines){
 		}
 		else {
 			if (lines[i][4] && lines[i][3]) {
-				lp = grade_array.indexOf(lines[i][4].toUpperCase())-grade_array.indexOf(lines[i][3].toUpperCase())
+				lp = initial_result[i-1];
 				levelsProgress.appendChild(document.createTextNode(lp));
 			}
 		}
@@ -414,10 +529,27 @@ for (i=1; i<lines.length-1; i++) {
 		if (lines[i][3]=== undefined) {
 		}
 		else {
-		a = grade_array.indexOf(lines[i][3].toUpperCase());
-		b = grade_array.indexOf(lines[i][4].toUpperCase());
-		v = (a+193)*(b-7);
+			if(isInArray(lines[i][4].toUpperCase(),grade_array) == true) {
+				var b = grade_array.indexOf(lines[i][4].toUpperCase())
+			}
+			else if (isInArray(lines[i][4],aps_array) == true) {
+				var b = aps_array.indexOf(lines[i][4]);
+			}
+			else {
+			b = 0
+			}
+			if(isInArray(lines[i][3].toUpperCase(),grade_array) == true) {
+				var a = grade_array.indexOf(lines[i][3].toUpperCase())
+			}
+			else if (isInArray(lines[i][3],aps_array) == true) {
+				var a = aps_array.indexOf(lines[i][3]);
+			}
+			else {
+			a = 0
+			}
+		v = (a+202)*(b+2);
 		arr[v].push(lines[i][0]+" "+lines[i][1]);
+		
 		}
 	 }
 
@@ -441,12 +573,12 @@ n=0;
 	firstNameCell.className = 'tm_layout';
 	}
 	else if (j==201 && i!=1) {
-	firstNameCell.appendChild(document.createTextNode(grade_array[i+7]));
+	firstNameCell.appendChild(document.createTextNode(grade_array[i-2]));
 	firstNameCell.className = 'tm_layout';
 	}
 	else {
 	if (i==1) {
-	firstNameCell.appendChild(document.createTextNode(grade_array[j-193]));
+	firstNameCell.appendChild(document.createTextNode(grade_array[j-202]));
 	firstNameCell.className = 'tm_layout';
 	}
 	else {
@@ -492,19 +624,25 @@ n=0;
 	}
 	
 	document.getElementById("tm_1_2").appendChild(table);
+	
 	}
 	
 	function printData()
 {
   //Get the HTML of div
-        
-        var divElements = document.getElementById("tm_1_2").innerHTML;
+        window.print();
+
+}
+
+function printTable() {
+
+ var divElements = document.getElementById("output").innerHTML;
         //Get the HTML of whole page
         var print_window = window.open();
 		
         //Reset the page's HTML with div's HTML only
         print_window.document.write( 
-          "<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"css/main.css\" /><link rel=\"stylesheet\" type=\"text/css\" href=\"css/bootstrap/bootstrap.min.css\" /><title></title></head><body>" + 
+          "<html><head><title></title></head><body>" + 
           divElements + "</body>");
 
         //Print Page
@@ -513,12 +651,6 @@ n=0;
 
 }
 
-$('#print_tm').on('click',function(){ printData();})
-
 
 
 	
-	$('body').tooltip({
-	
-    selector: '[id=tooltip]'
-	});
